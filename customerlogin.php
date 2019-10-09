7c0715b6f3cad0c972fb7b3653d1a9b39fa4f88d
bilal123456 <bilalraza203@gmail.com>
2019-10-09 12:53:04 +0500

    mailer

--- customerlogin.php
+++ customerlogin.php
@@ -1,6 +1,13 @@
 <?php
 include('Admin/Database/Connection.php');
 session_start();
+		
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'mail/src/Exception.php';
+require 'mail/src/PHPMailer.php';
+require 'mail/src/SMTP.php';
 
 if(isset($_POST['customerlogin']))
 
@@ -41,13 +48,53 @@ $vkey = md5(time() . $username);
 		$runinsert = mysqli_query($link,$insert);
 		if($runinsert)
 		{
-			$to = $email;
-			$subject = "Email Verfication";
-			$message = "<a href='http://localhost/registration/verifiy.php?vkey=$vkey'>Register Account</a>";
-			$headers = "From:CryptoCurrency@yahoo.com";
-			$headers = "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=UTF-8" ."\r\n";
-			mail($to,$subject,$message,$headers);
+
+
+
+
+
+
+
+// Load Composer's autoloader
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 4;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'bilalraza203@gmail.com';                     // SMTP username
+    $mail->Password   = 'king5872123';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('bilalraza203@gmail.com', 'Professional');
+    $mail->addAddress('bilalraza203@gmail.com', 'user email');     // Add a recipient
+    $mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('bilalraza203@gmail.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Please Click here the link below';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 		}
 		if($rafferal !== "" && $rafferalref !="")
 		{
